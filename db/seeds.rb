4a91cadac1411a5a05b983ad9b2b3e82dd8f4eea
Kevin Hoiland <KevinAndrewHoiland@gmail.com>
2013-04-21 17:24:35 -0700

    ugh seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
+=begin
 # Create default admin user
   AdminUser.create!([:email => 'kevin.hoiland@intlgum.com', :password => '1ntlP@s54kevho', :password_confirmation => '1ntlP@s54kevho'], :without_protection => true)
+=end
 
 # Create default dynamic text
-File.open("db/initial_text_final.txt", "r") do |defaults|
+File.open("db/initial_text3", "r") do |defaults|
   defaults.read.each_line do |text|
     location, sequence, size, visible, content = text.chomp.split("|")
     DynamicText.create!([:location => location, :sequence => sequence, :size => size, :visible => visible, :content => content], :without_protection => true)
   end
 end
+
+
 =begin
 # Create default set of gums
 File.open("db/initial_gums6_mini.txt", "r") do |gums|
