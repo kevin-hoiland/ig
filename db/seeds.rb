ca35965cea2b1aa41fa743bffb6d4b41e5230745
Kevin Hoiland <KevinAndrewHoiland@gmail.com>
2013-04-21 17:16:34 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,13 @@
   AdminUser.create!([:email => 'kevin.hoiland@intlgum.com', :password => '1ntlP@s54kevho', :password_confirmation => '1ntlP@s54kevho'], :without_protection => true)
 
 # Create default dynamic text
-File.open("db/initial_text3.txt", "r") do |defaults|
+File.open("db/initial_text_final.txt", "r") do |defaults|
   defaults.read.each_line do |text|
     location, sequence, size, visible, content = text.chomp.split("|")
     DynamicText.create!([:location => location, :sequence => sequence, :size => size, :visible => visible, :content => content], :without_protection => true)
   end
 end
-
+=begin
 # Create default set of gums
 File.open("db/initial_gums6_mini.txt", "r") do |gums|
   gums.read.each_line do |gum|
@@ -16,3 +16,4 @@ File.open("db/initial_gums6_mini.txt", "r") do |gums|
     Gum.create!([:permalink => permalink, :title => title, :upc => upc, :active => active, :discontinued => discontinued, :company => company, :brand => brand, :flavor => flavor, :description => description, :note => note, :country => country, :new_release => new_release, :image => File.open(File.join(Rails.root, '/lib/assets/images/gums/'+image))], :without_protection => true)
   end
 end
+=end
