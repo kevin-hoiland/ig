01746053692f057a91a0145106e5d524e6fad229
Kevin Hoiland <KevinAndrewHoiland@gmail.com>
2013-05-02 19:48:39 -0700

    Bug Fix:  Gum List page no longer displays duplicate gums when sorting by Votes

--- app/controllers/gums_controller.rb
+++ app/controllers/gums_controller.rb
@@ -10,7 +10,7 @@ class GumsController < ApplicationController
   def list
     @content_legal = DynamicText.content("gum_general").order("sequence ASC")
     @search_gums = Gum.active.ransack(params[:q])
-    @gums_list = @search_gums.result.order('RAND('+session[:seed]+')').page(params[:page])
+    @gums_list = @search_gums.result(:distinct => true).order('RAND('+session[:seed]+')').page(params[:page])
     @country_list = Gum.active.select(:country).uniq
   end
   
