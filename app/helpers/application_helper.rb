71b01b111da4463b2faf19067a36ea67bab8d456
Kevin Hoiland <KevinAndrewHoiland@gmail.com>
2013-05-02 19:42:41 -0700

    SEO:  removed legal context from Title and Descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   
   def title(page_title)
-    content_for(:title) { page_title.html_safe }
+    content_for(:title) { remove_legals(page_title).html_safe }
   end
   
   def description(page_description)
-    content_for(:description) { page_description.html_safe }
+    content_for(:description) { remove_legals(page_description).html_safe }
   end
   
   def permalink_humanize(permalink)
@@ -16,6 +16,12 @@ module ApplicationHelper
     map = {'(TM)' => '&#8482;', '(R)' => '&#174;', '(C)' => '&#169;'}
     re = Regexp.new(map.keys.map { |x| Regexp.escape(x) }.join('|'))
     text.gsub(re, map).html_safe
+  end
+  
+  def remove_legals(text)
+    map = {'(TM)' => '', '(R)' => '', '(C)' => ''} # note for SEO, removing legal context in titles. also note, if (TM) or other has spaces on both sides, will result in double space :(
+    re = Regexp.new(map.keys.map { |x| Regexp.escape(x) }.join('|'))
+    text.gsub(re, map)    
   end
     
 end
