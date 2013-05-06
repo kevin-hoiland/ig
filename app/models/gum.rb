e8a69a733ed4bb6feed1abe19ca8d829a66935e8
Kevin Hoiland <KevinAndrewHoiland@gmail.com>
2013-05-06 09:20:01 -0700

    tons of layout improvements, removed sorts etc
    gums list:  title font size increase, removed all sorting
    gum show: changed link text to "Ratings List"
    per gum list: swapped link options order
    all ratings: fixed rank5 search text, improved comment search, and
    removed sort by total
    gum model:  fixed relationship spelling error for has_many profiles...,
    also commented out test code for gums list averages etc

--- app/models/gum.rb
+++ app/models/gum.rb
@@ -6,7 +6,7 @@ class Gum < ActiveRecord::Base
   
   ############  attributes  ############
   attr_accessible :permalink, :title, :upc, :active, :company, :brand, :flavor, :description, :note, :country, :new_release, :discontinued, :image, as: :admin
-  
+    
   #from : http://erniemiller.org/2012/05/11/why-your-ruby-class-macros-might-suck-mine-did/
   def self.ransackable_attributes(auth_object = nil)
 #    super & ['upc', 'title', 'company', 'active', 'brand', 'flavor', 'country', 'description', 'new_release', 'testing', 'order_by_score_asc', 'order_by_score_desc', 'score', 'omg']
@@ -20,7 +20,7 @@ class Gum < ActiveRecord::Base
   ############  associations  ############
   has_many :gum_rating_relationships # looks like i also probably want ", :dependent => true", but not allowing gum deletions yet...
   has_many :gum_shipment_relationships
-  has_many :profiles, :through => :gum_rating_relationsihps
+  has_many :profiles, :through => :gum_rating_relationships #this was spelled wrong!?!?!?!
   has_and_belongs_to_many :shipments, :join_table => :gum_shipments
   has_many :votes
   #  accepts_nested_attributes_for :gum_rating_relationships #allow_destroy => true
@@ -43,7 +43,7 @@ class Gum < ActiveRecord::Base
 # https://github.com/gregbell/active_admin/pull/1994
   
   ############  methods  ############
-
+  
   def total_average_rating_to_score (gum_id)
     set = GumRatingRelationship.where(:gum_id => gum_id)
     if set.empty?
@@ -56,7 +56,31 @@ class Gum < ActiveRecord::Base
   def to_score
     (self.to_i*2).to_s+"%"
   end
+
+=begin  
+  def get_gum_list_rating(gum_id, rating_type)
+    ranking_set = GumRatingRelationship.where(:gum_id => gum_id)
+    case rating_type
+      when "rank_1"
+        @rank_1_average = ranking_set.average(:rank_1)
+        return ranking_set.average(:rank_1)
+      when "rank_2"
+        return ranking_set.average(:rank_2)
+      when "rank_3"
+        return ranking_set.average(:rank_3)
+      when "rank_4"
+        return ranking_set.average(:rank_4)
+      when "rank_5"
+        return ranking_set.average(:rank_5)
+    end      
+  end
+=end
   
+#  def rank_1_average
+#    GumRatingRelationship.where(:gum_id => self.id).average(:rank_1)
+#    GumRatingRelationship.average :rank_1, :conditions => ['gum_id = ?', self.id]
+#  end
+    
   def get_rating_averages
     set = GumRatingRelationship.where(:gum_id => self.id)
     return([set.average(:rank_1), set.average(:rank_2), set.average(:rank_3), set.average(:rank_4), set.average(:rank_5) ])
