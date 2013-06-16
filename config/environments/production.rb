0c463d3dab246213d32d0f6d2e6408a01a5fc026
Kevin Hoiland <KevinAndrewHoiland@gmail.com>
2013-06-16 09:38:23 -0700

    bug fix (hopefully) - sign up/out/in issues with ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 IntlGum2::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
-  config.to_prepare { Devise::SessionsController.force_ssl }
-  config.to_prepare { Devise::RegistrationsController.force_ssl }
+#  config.to_prepare { Devise::SessionsController.force_ssl }
+#  config.to_prepare { Devise::RegistrationsController.force_ssl }
   
   # Code is not reloaded between requests
   config.cache_classes = true
